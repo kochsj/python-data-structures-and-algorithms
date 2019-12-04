977e2e0af3c9997665c005b7a1bd7fc5fa4ad3af
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-04 14:34:18 -0800

    added three more tests to test non integer values and lists. expected == actual.

--- challenges/array_binary_search/test_array_binary_search.py
+++ challenges/array_binary_search/test_array_binary_search.py
@@ -1,11 +1,26 @@
 from array_binary_search import binary_search
 
-def test_one_value_not_in_list():
+def test_one_no_parameters():
+    expected = -1
+    actual = binary_search()
+    assert expected == actual
+
+def test_two_value_not_in_short_list():
     expected = -1
     actual = binary_search([15], 9)
     assert expected == actual
 
-def test_two_no_parameters():
+def test_three_value_not_in_longer_list():
     expected = -1
-    actual = binary_search()
+    actual = binary_search([4,8,15,16,23,42], 90)
+    assert expected == actual
+
+def test_four_value_not_in_mixed_type_list():
+    expected = -1
+    actual = binary_search(['apple', 14, True, 'Thor', None], 90)
+    assert expected == actual
+
+def test_five_non_integer_value():
+    expected = -1
+    actual = binary_search([4,8,15,16,23,42], 'apple')
     assert expected == actual
