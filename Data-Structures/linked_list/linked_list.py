5d45beb609813166a54a897173e80e8776143132
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-06 22:33:13 -0800

    created linked_list class and node class with 'pass'

---
+++ Data-Structures/linked_list/linked_list.py
@@ -0,0 +1,19 @@
+class Linked_List:
+
+    def __init__(self, head):
+        self.head = head
+        self.linked_list = []
+    def __repr__(self):
+        pass
+    def __str__(self):
+        pass
+    def insert(self):
+        pass
+    def includes(self):
+        pass
+
+class Node:
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
---
+++ Data-Structures/linked_list/test_linked_list.py
@@ -0,0 +1,1 @@
+from linked_list import Linked_List
