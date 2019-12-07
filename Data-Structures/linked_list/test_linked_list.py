8a5c234369d8c124e7e180ef555086ad0a76279f
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-07 11:54:54 -0700

    created tests for inserting two and many items into a linked list. the tests pass if the most recent item is linked as the head and the most recent node's next pointer points to the previous head node.

--- Data-Structures/linked_list/test_linked_list.py
+++ Data-Structures/linked_list/test_linked_list.py
@@ -9,10 +9,25 @@ def test_instance_of_node():
     assert Node('Node_1').value == 'Node_1'
 def test_one_item_in_list():
     empty_list = Linked_List()
-        # insert_into_list = empty_list.insert('Node_1')
     empty_list.insert('Node_1')
     assert empty_list.head and Node('Node_1').next == None
+def test_two_items_into_new_list():
+    empty_list = Linked_List()
+    empty_list.insert('Node_1')
+    empty_list.insert('Node_2')
+    assert str(empty_list.head) == 'Node_2'
+def test_many_items_into_new_list():
+    empty_list = Linked_List()
+    empty_list.insert('Node_1')
+    empty_list.insert('Node_2')
+    empty_list.insert('Node_3')
+    empty_list.insert('Node_4')
+    empty_list.insert('Node_5')
+    empty_list.insert('Node_6')
+    assert str(empty_list.head) == 'Node_6' and str(empty_list.head.next) == 'Node_5'
+
 # The head property will properly point to the first node in the linked list
+
 # Can properly insert multiple nodes into the linked list
 # Will return true when finding a value within the linked list that exists
 # Will return false when searching for a value in the linked list that does not exist
