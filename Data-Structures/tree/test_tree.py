e206671c6364c9541f092b7e90ef32df296d1d5f
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-19 15:21:53 -0800

    wrote tests for pre_order method

--- Data-Structures/tree/test_tree.py
+++ Data-Structures/tree/test_tree.py
@@ -2,15 +2,42 @@ import pytest
 from tree import _Node, BinaryTree, BinarySearchTree
 
 
+################################################################################
+# PYTEST FIXTURES FOR TESTING                                                  #
+################################################################################
+@pytest.fixture()
+def binary_search_tree_ten():
+    new_tree = BinarySearchTree()
+    new_tree.add(10)
+    new_tree.add(5)
+    new_tree.add(20)
+    new_tree.add(3)
+    new_tree.add(100)
+    new_tree.add(1)
+    new_tree.add(15)
+    new_tree.add(13)
+    new_tree.add(2)
+    new_tree.add(90)
+    return new_tree
+    
+################################################################################
+# Can successfully instantiate an empty tree                                   #
+################################################################################
 def test_binary_search_tree_instance():
     new_tree = BinarySearchTree()
     assert new_tree.root == None
 
+################################################################################
+# Can successfully instantiate a tree with a single root node                  #
+################################################################################
 def test_binary_search_tree_add_one():
     new_tree = BinarySearchTree()
     new_tree.add(10)
     assert new_tree.root.value == 10
 
+#################################################################################
+# Can successfully add a left children and right children to a single root tree #
+#################################################################################
 def test_binary_search_tree_add_three():
     new_tree = BinarySearchTree()
     new_tree.add(10)
@@ -19,3 +46,54 @@ def test_binary_search_tree_add_three():
     assert new_tree.root.value == 10
     assert new_tree.root.right.value == 20
     assert new_tree.root.left.value == 5
+
+def test_binary_search_tree_add_six():
+    new_tree = BinarySearchTree()
+    new_tree.add(10)
+    new_tree.add(5)
+    new_tree.add(20)
+    new_tree.add(2)
+    new_tree.add(7)
+    new_tree.add(25)
+    assert new_tree.root.value == 10
+    assert new_tree.root.right.right.value == 25
+    assert new_tree.root.left.left.value == 2
+    assert new_tree.root.left.right.value == 7
+
+def test_binary_search_tree_add_ten():
+    new_tree = BinarySearchTree()
+    new_tree.add(10)
+    new_tree.add(5)
+    new_tree.add(20)
+    new_tree.add(3)
+    new_tree.add(100)
+    new_tree.add(1)
+    new_tree.add(15)
+    new_tree.add(13)
+    new_tree.add(2)
+    new_tree.add(90)
+    assert new_tree.root.value == 10
+    assert new_tree.root.right.right.left.value == 90
+    assert new_tree.root.left.left.left.right.value == 2
+
+#################################################################################
+# Can successfully return a collection from a pre_order traversal               #
+#################################################################################
+@pytest.mark.skip()
+def test_pre_order_ten(binary_search_tree_ten):
+    expected = [10,5,3,1,2,20,15,13,100,90]
+    assert binary_search_tree_ten.pre_order(binary_search_tree_ten.root) == expected
+#################################################################################
+# Can successfully return a collection from an in_order traversal               #
+#################################################################################
+@pytest.mark.skip()
+def test_in_order_ten(binary_search_tree_ten):
+    expected = [1,3,2,5,10,13,15,90,100]
+    assert binary_search_tree_ten.in_order() == expected
+#################################################################################
+# Can successfully return a collection from a post_order traversal              #
+#################################################################################
+@pytest.mark.skip()
+def test_post_order_ten(binary_search_tree_ten):
+    expected = [2,1,3,5,13,15,20,90,100,10]
+    assert binary_search_tree_ten.post_order() == expected
--- Data-Structures/tree/tree.py
+++ Data-Structures/tree/tree.py
@@ -1,21 +1,57 @@
 class _Node:
+    """
+    Creates an instance of a node.
+    Has value, left, and right properties.
+    """
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
 
 class BinaryTree:
+    """
+    Creates an instance of a Binary tree.
+    Has a root property.
+    Has three methods: pre_order, in_order, and post_order.
+    """
     def __init__(self):
         self.root = None
-    def pre_order(self):
+
+    def pre_order(self, root, arr=[]):
+        """
+        Method that takes no parameters.
+        Returns an array of the values, ordered from the start, going far left, then finishing to the right.
+        """
+        arr.append(root.value)
+
+        if root.left:
+            self.pre_order(root.left, arr)
+
+        if root.right:
+            self.pre_order(root.right, arr)
+
+    def post_order(self, arr=[]):
+        """
+        Method that takes no parameters.
+        Returns an array of the values, ordered starting from the far left, traversing to the top, then finishing to the right.
+        """
         pass
-    def post_order(self):
-        pass
-    def in_order(self):
+    def in_order(self, arr=[]):
+        """
+        Method that takes no parameters.
+        Returns an array of the values, ordered starting from the far left, traversing to the far right, then finishing to at the root.
+        """
         pass
 
 class BinarySearchTree(BinaryTree):
     def add(self, value):
+        """
+        Method that accepts one value.
+        Adds a node to the binary search tree
+        Conditionals that check if the node's value is greater than or less than the "top" node
+        Greater values go to the right. Lesser values go to the left.
+        Continues until leaf is hit
+        """
         node = _Node(value)
         if not self.root:
             self.root = node
@@ -32,4 +68,12 @@ class BinarySearchTree(BinaryTree):
                 else:
                     top.right = node
                     return
+    
+    def contains(self, value):
+        """
+        Method that accepts one value.
+        Traverses the tree untill it reaches a node with the value that was sent in as an arguement
+        Returns True or False if the value is in the tree at least once
+        """
+
                     
