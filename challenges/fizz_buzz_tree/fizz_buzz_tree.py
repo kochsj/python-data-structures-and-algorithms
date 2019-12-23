8da634f8ffc1466d077906e1ac55b1b82d484371
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-23 15:01:34 -0800

    created simple _Node and BinaryTree classes. One method for binary tree (add)

---
+++ challenges/fizz_buzz_tree/fizz_buzz_tree.py
@@ -0,0 +1,31 @@
+class _Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def add(self, value, root=None):
+        node = _Node(value)
+        root = self.root
+
+        if not root:
+            self.root = node
+            return
+
+        if value < root.value:
+            if root.left:
+                self.add(value, root.left)
+            else:
+                root.left = node
+        if value > root.value:
+            if root.right:
+                self.add(value, root.right)
+            else:
+                root.right = node
+                        
+            
+
---
+++ challenges/fizz_buzz_tree/test_fizz_buzz_tree.py
@@ -0,0 +1,3 @@
+import pytest
+from fizz_buzz_tree import BinaryTree, fizz_buzz_func, fizz_buzz_tree, _Node
+
