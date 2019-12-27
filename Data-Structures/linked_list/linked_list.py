be3f6d9ca2f41f50a6f4a9aaf3224bce2acaa393
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-27 15:30:09 -0800

    wrote in missing docstrings. rewrote insert_before/after and append.

--- Data-Structures/linked_list/linked_list.py
+++ Data-Structures/linked_list/linked_list.py
@@ -4,8 +4,8 @@ class LinkedList:
     Optional parameter: head
         Head = node address (value)
     """
-    def __init__(self, head=None):
-        self.head = head
+    def __init__(self):
+        self.head = None
 
     def __repr__(self):
         return "This is a linked list."
@@ -32,13 +32,16 @@ class LinkedList:
         """
         current = self.head
         while current:
-            if str(current) == value:
+            if str(current.value) == value:
                 return True
             else:
                 current = current.next_node
         return False
 
     def return_list(self):
+        """
+        Returns a list of all of the nodes in the linked list. The actual nodes themselves.
+        """
         try:
             current = self.head
             collection_of_values = []
@@ -51,37 +54,73 @@ class LinkedList:
 
     # .append(value) which adds a new node with the given value to the end of the list
     def append(self, value):
+        """
+        Method that takes a value as its only arguement.
+        Creates a Node object.
+        Adds the new node to the end of the linked list.
+        """
         node = Node(value)
-        current = self.head
-        while current and current.next_node != None:
-            current = current.next_node
-        current.next_node = node
+        if self.head:
+            current = self.head
+            while current:
+                if current.next_node:
+                    current = current.next_node
+                else:
+                    break
+            current.next_node = node
+
+        else:
+            self.head = node
 
     # .insertBefore(value, newVal) which add a new node with the given newValue immediately before the first value node
     def insert_before(self, value, new_value):
+        """
+        Method that takes a targeted node value and a new node value as arguements.
+        Creates a new node from the new_value.
+        Traverses the linked list and stops when it finds the targeted node value.
+        Inserts the new node into the linked list BEFORE the targed node value.
+        """
         node = Node(new_value)
-        current = self.head
-        if current.value == value:
-            node.next_node = self.head
-            self.head = node
-        while current.next_node and self.head.value != new_value:
-            if current.next_node.value != value:
+        if self.head:
+            if self.head.value == value:
+                node.next_node = self.head
+                self.head = node
+                return
+            current = self.head
+            while current:
+                if current.next_node.value == value:
+                    node.next_node = current.next_node
+                    current.next_node = node
+                    return
                 current = current.next_node
-            else:
-                node.next_node = current.next_node
-                current.next_node = node
-                break
+        else:
+            return "Not found"
+
 
     # .insertAfter(value, newVal) which add a new node with the given newValue immediately after the first value node
     def insert_after(self, target, new_value):
+        """
+        Method that takes a targeted node value and a new node value as arguements.
+        Creates a new node from the new_value.
+        Traverses the linked list and stops when it finds the targeted node value.
+        Inserts the new node into the linked list AFTER the targed node value.
+        """
         node = Node(new_value)
-        current = self.head
-        while current.value != target:
-            current = current.next_node
-        node.next_node = current.next_node
-        current.next_node = node
+        if self.head:
+            current = self.head
+            while current:
+                if current.value == target:
+                    node.next_node = current.next_node
+                    current.next_node = node
+                current = current.next_node
+        else:
+            return "Not found"
 
     def kth_from_end(self, k):
+        """
+        Traverses the linked list.
+        Returns the node value k-indexes from the end of the linked list.
+        """
         value_list = []
         current = self.head
         while current.next_node:
@@ -94,6 +133,10 @@ class LinkedList:
             return 'K is out of range'
 
     def find_middle_node(self):
+        """
+        Traverses the linked list.
+        Returns the value of the node in the middle of the linked list (ceiling).
+        """
         value_list = []
         current = self.head
         while current.next_node:
@@ -110,9 +153,9 @@ class Node:
     Next_node = value pointing to the next node
     Value = value held in the node
     """
-    def __init__(self, value, next_node=None):
+    def __init__(self, value):
         self.value = value
-        self.next_node = next_node
+        self.next_node = None
 
     def __repr__(self):
         return self.value
--- Data-Structures/linked_list/test_linked_list.py
+++ Data-Structures/linked_list/test_linked_list.py
@@ -1,6 +1,126 @@
 import pytest
 from linked_list import Node, LinkedList
 
+
+
+@pytest.fixture
+def node():
+    return Node(42)
+
+
+@pytest.fixture
+def ll():
+    return LinkedList()
+
+
+@pytest.fixture
+def filled_ll():
+    ll = LinkedList()
+    ll.insert(42)
+    ll.insert(14)
+    ll.insert('October')
+    return ll
+
+
+def test_exists():
+    assert LinkedList
+    assert Node
+
+
+def test_node_next(node):
+    assert node.next_node == None
+
+
+def test_node_value(node):
+    assert node.value == 42
+
+
+def test_ll_head(ll):
+    assert ll.head == None
+
+
+def test_ll_insert_one(ll):
+    ll.insert(42)
+    assert ll.head.value == 42
+
+
+def test_ll_insert_two(ll):
+    ll.insert(42)
+    ll.insert(14)
+    assert ll.head.value == 14
+    assert ll.head.next_node.value == 42
+
+
+def test_ll_insert_three(ll):
+    ll.insert(42)
+    ll.insert(14)
+    ll.insert('October')
+    assert ll.head.value == 'October'
+    assert ll.head.next_node.value == 14
+    assert ll.head.next_node.next_node.value == 42
+
+
+def test_ll_includes(filled_ll):
+    assert filled_ll.includes('October') == True
+    assert filled_ll.includes(2001) == False
+    # Edge Case: Checking an empty linked list
+    assert LinkedList().includes('Towel') == False
+
+
+def test_ll_append(ll, filled_ll):
+    # Append Once
+    ll.append(2)
+    assert ll.head.value == 2
+
+    # Append Multiple
+    ll.append(4)
+    assert ll.head.next_node.value == 4
+
+    filled_ll.append(2)
+    assert filled_ll.head.next_node.next_node.next_node.value == 2
+
+
+def test_ll_insert_before(ll, filled_ll):
+    assert ll.head == None
+    # Empty List
+    ll.insert_before(2, 6)
+    assert ll.head == None
+
+    # Single Item List
+    ll.append(2)
+    ll.insert_before(2, 1)
+    assert ll.head.value == 1
+
+    # Target Not Present
+    ll.insert_before(1, 3)
+    assert ll.head.value == 3
+
+    # Filled List
+    filled_ll.insert_before(14, 'howdy')
+    assert filled_ll.head.next_node.value == 'howdy'
+
+
+def test_ll_insert_after(ll, filled_ll):
+    # Empty List
+    ll.insert_after(2, 6)
+    assert ll.head == None
+
+    # Single Item List
+    ll.append(2)
+    ll.insert_after(2, 1)
+    assert ll.head.next_node.value == 1
+
+    # Target Not Present
+    ll.insert_after(3, 4)
+    assert ll.head.value == 2
+    assert ll.head.next_node.value == 1
+    assert ll.head.next_node.next_node == None
+
+    # Filled List
+    filled_ll.insert_after(42, 'howdy')
+    assert filled_ll.head.next_node.next_node.next_node.value == 'howdy'
+
+
 # creates linked lists of varrying numbers of nodes:
 # one node
 @pytest.fixture()
