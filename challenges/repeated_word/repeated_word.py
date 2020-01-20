a84d2cc3aa7d022e081c8429fe80384098774e3b
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-01-20 14:16:44 -0800

    refactored repeated word to use just the dictionary, not an extra list.

--- challenges/repeated_word/repeated_word.py
+++ challenges/repeated_word/repeated_word.py
@@ -5,22 +5,22 @@ import re
 def repeated_word(string):
     string = re.sub(r'[^\w\s]', '', string)
     words = {}
-    processed_words = []
+    first_matching_word = ''
     most_frequent_words = []
 
 # Modify your function to return a count of each of the words in the provided string
     for word in string.split():
-        if words[word]:
+        try:
             words[word] += 1
-        else: words[word] = 1
+            first_matching_word = first_matching_word or word
+        except:
+            words[word] = 1
 
 # Modify your function to return a list of the words most frequently used in the provided string
     for key in words:
         most_frequent_words.append((words[key], key))
     most_frequent_words.sort(reverse=True)
-
-    for word in string.split():
-        if word in processed_words:
-            return [word, words, most_frequent_words]
-        processed_words.append(word)
+    
+    if first_matching_word:
+        return [first_matching_word, words, most_frequent_words]
     return ["No repeating words", words, most_frequent_words]
--- challenges/repeated_word/test_repeated_word.py
+++ challenges/repeated_word/test_repeated_word.py
@@ -1,1 +1,7 @@
-ECHO is off.
+import pytest
+from repeated_word import repeated_word
+
+def test_simple_string():
+    string = "Once upon a time, there was a brave princess who.."
+    lst = repeated_word(string)
+    assert lst[0] == 'a'
