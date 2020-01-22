65da5ab53e3fea4116d70442450e61adea472a2d
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-01-21 22:20:36 -0800

    added two more tests for edge cases

--- challenges/tree_intersection/test_tree_intersection.py
+++ challenges/tree_intersection/test_tree_intersection.py
@@ -58,3 +58,35 @@ def test_multiple_matching_values_in_one_tree():
     tree_two.add(15)
 
     assert tree_intersection(tree_one, tree_two) == []
+
+def test_everything_matches():
+    tree_one = BinaryTree()
+    tree_one.add(12)
+    tree_one.add(5)
+    tree_one.add(3)
+    tree_one.add(15)
+
+    tree_two = BinaryTree()
+    tree_two.add(12)
+    tree_two.add(5)
+    tree_two.add(3)
+    tree_two.add(15)
+    assert tree_intersection(tree_one, tree_two) == [15, 5, 3, 12]
+
+def test_inner_contained_tree():
+    tree_one = BinaryTree()
+    tree_one.add(55)
+    tree_one.add(12)
+    tree_one.add(6)
+    tree_one.add(5)
+    tree_one.add(3)
+    tree_one.add(100)
+    tree_one.add(23)
+    tree_one.add(1)
+
+    tree_two = BinaryTree()
+    tree_two.add(12)
+    tree_two.add(5)
+    tree_two.add(3)
+
+    assert tree_intersection(tree_one, tree_two) == [5, 3, 12]
