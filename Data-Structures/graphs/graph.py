f7fa479bdec2bfd3d0b2de9ac799412997bc75ee
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2020-01-25 17:27:54 -0800

    wrote graph class and tests

--- Data-Structures/graphs/graph.py
+++ Data-Structures/graphs/graph.py
@@ -2,9 +2,10 @@ class Graph:
     """
     [method] - add_node(value): adds a new vertex to the graph, returns the added vertex
     [method] - add_edge(vertex1, vertex2, [weight]): adds new edge between two virtices, takes in two verticies, has ability to add weight
-    [method] - get_nodes - returns all of the vertices as a collection
+    [method] - get_nodes() - returns all of the vertices as a collection
     [method] - get_neighbors(vertex): returns a collection of vertices (with weights) connected to a vertex, takes in a vertex
-
+    [method] - get_values(vertex): returns the value from a given vertex
+    [method] - size() - returns number of vertices in Graph; integer
     """
     def __init__(self):
         self._adj_list = {}
@@ -18,10 +19,10 @@ class Graph:
             vertex - the vertex that was created
         """
         vertex = Vertex(value)
-        try:
-            self._adj_list[vertex].append(vertex)
+        try: 
+            self._adj_list[vertex].append([vertex])
         except:
-            self._adj_list[vertex] = [vertex]
+            self._adj_list[vertex] = [[vertex]]
 
         return vertex
 
@@ -36,10 +37,13 @@ class Graph:
         """
         if start_vertex not in self._adj_list or end_vertex not in self._adj_list:
             raise KeyError(f'{start_vertex.value} is not in {self}')
-        
-        adjacencies = self._adj_list[start_vertex]
-        adjacencies.append((end_vertex, weight))
+    
+        adj_list = self._adj_list[start_vertex]
+        for i in range(len(adj_list)):
+            adjacencies = adj_list[i]
 
+            if adjacencies[0] == start_vertex:
+                adjacencies += [end_vertex, weight]
 
 
     def size(self):
@@ -48,21 +52,47 @@ class Graph:
         """
         return len(self._adj_list)
 
-    def get_value(self, vertex):
+    def get_values(self, vertex):
         """
-        Gets a value from the graph at a given vertex
+        Gets a values from the graph at a given vertex
         Parameters:
             [vertex] - The vertex to get value from
         Return:
-            value - value from the vertex
+            values - values from the vertex
         """
+        try:
+            for vtx in self._adj_list[vertex]:
+                if vtx[0] == vertex:
+                    return vtx[0].value
+            return None
+        except:
+            raise KeyError(f'{vertex} is not in {self}')
 
+    def get_nodes(self):
+        """ Returns a list of the vertices in the Graph."""
+        return [vtx for vtx in self._adj_list]
 
+    def get_neighbors(self, vertex):
+        """
+        Gets the neighboring vertices to a given vertex
+        Parameters:
+            [vertex] - vertex to get the neighbors of
+        Return:
+            [list] - list of vertices with weights
+        """
+        try:
+            lst = [vtx[idx] for vtx in self._adj_list[vertex] for idx in range(1,len(vtx))]
+            if lst:
+                return lst
+            return None
+        except:
+            return None
 
 class Vertex:
     """
-    Instance of a vertex
-    [init] - requires a value to init
+    Instance of a vertex object
+    Attributes:
+        [value] - requires a value to init
     """
     def __init__(self, value):
         self.value = value
--- Data-Structures/graphs/test_graph.py
+++ Data-Structures/graphs/test_graph.py
@@ -35,6 +35,29 @@ def test_edge_is_added():
     
     graph.add_edge(start, end, 3)
 
-    assert graph._adj_list[start][0].value == 'start'
-    assert graph._adj_list[start][1] == (end, 3)
+    assert graph._adj_list[start][0] == [start, end, 3]
 
+def test_get_nodes():
+    graph = Graph()
+    start = graph.add_node('start')
+    end = graph.add_node('end')
+
+    assert graph.get_nodes() == [start, end]
+
+def test_get_neighbors():
+    graph = Graph()
+    start = graph.add_node('start')
+    end = graph.add_node('end')
+
+    graph.add_edge(start, end, 3)
+
+    assert graph.get_neighbors(start) == [end, 3]
+    assert graph.get_nodes() == [start, end]
+
+def test_get_values():
+    graph = Graph()
+    start = graph.add_node('start')
+    end = graph.add_node('end')
+
+    assert graph.get_values(start) == 'start'
+    
